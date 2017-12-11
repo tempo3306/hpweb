24092cad899c1c1c20de7852507807d441fe61ac
zs <810909753@qq.com>
2017-12-11 11:38:55 +0800

    1211

--- main_api_1_0/api_views.py
+++ main_api_1_0/api_views.py
@@ -9,9 +9,22 @@ from flask import jsonify,request
 import sqlalchemy
 from ..models import User, Role, Permission, Auction, Action
 
+
+
+
+
 @main_api.route('/userconfirm/info',methods=['GET'])
 def userconfirm():
-    print("fdsf")
+    import time
+    time1 = time.localtime(time.time())
+    time2 = time.strftime("%Y%m%d", time1)
+    today_date = "time2" + "01"
+    url_dianxin = "https://paimai2.alltobid.com/bid/%s/login.htm" % today_date
+    url_nodianxin = "https://paimai.alltobid.com/bid/%s/login.htm" % today_date
+
+    # url_dianxin = "https://paimai2.alltobid.com/bid/%s/login.htm" % today_date
+    # url_nodianxin = "https://paimai.alltobid.com/bid/%s/login.htm" % today_date
+
     username=request.args.get('username')
     passwd=request.args.get('passwd')
     user=User.query.filter_by(username=username).first()  #判断用户是否存在
@@ -19,8 +32,8 @@ def userconfirm():
         result=user.verify_password(passwd)
         if result:
             return jsonify({'result':'login success',
-                            'url_dianxin':'www.baidu.com',
-                            'url_nodianxin':'www.qq.com'})
+                            'url_dianxin':url_dianxin,
+                            'url_nodianxin':url_nodianxin})
         else:
             return jsonify({'result':'wrong password'})
     else:
